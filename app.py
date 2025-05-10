43f5f35d53f61645cf3d74e8373b016b3e0ec71b
Sukiciwir <rizqi9lagiskolah@gmail.com>
2025-05-10 17:31:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -667,6 +667,6 @@ with st.expander("📜 Riwayat Operasi"):
 # Footer
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown(
-    "<p style='text-align:center; color:#00bfff;'>Made with 💙 by <strong>Suki</strong> | Powered by Streamlit</p>",
+    "<p style='text-align:center; color:#00bfff;'>Made with 💙 by <strong>Suki</strong> | Powered by Streamlit | *There are still many bugs, you can report on my insta</p>",
     unsafe_allow_html=True,
 )
